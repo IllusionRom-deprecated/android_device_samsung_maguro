fbee14d895b09331ede82607f5bf818fb3e1ad2e
AdityaSureka <adityasureka92@gmail.com>
2014-05-21 07:22:26 -0400

    Illusion Ready

--- ill.mk
+++ ill.mk
@@ -5,21 +5,25 @@ PRODUCT_RELEASE_NAME := GN-GSM
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Illusion stuff.
+$(call inherit-product, vendor/illusion/config/common.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/illusion/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/maguro/aosp_maguro.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := maguro
-PRODUCT_NAME := cm_maguro
+PRODUCT_NAME := ill_maguro
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Galaxy Nexus
 PRODUCT_MANUFACTURER := samsung
 
 #Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="google/yakju/maguro:4.3/JWR66Y/776638:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.3 JWR66Y 776638 release-keys"
+
+# bootanimation
+PRODUCT_COPY_FILES += \
+	vendor/illusion/bootanimation/720x1280.zip:system/media/bootanimation.zip
